e63f118425e40aa698177ffa69da95761fe32834
jeffmufugi <mufugi.michelo@gmail.com>
2026-05-31 20:49:58 -0400

    fix 404 to 40444 in spectest

--- tests/404.spec.ts
+++ tests/404.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("404 Page", () => {
-  test("should return 404 for unknown route", async ({ page }) => {
+  test("should return 40444 for unknown route", async ({ page }) => {
     const response = await page.goto("/this-page-does-not-exist");
     expect(response?.status()).toBe(404);
   });

5c05eea2afc021a15acab7712a5d60b2a7f7b97a
jeffmufugi <mufugi.michelo@gmail.com>
2026-06-02 15:33:43 -0400

    fix: fixes meta data in layout file and update tag

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from "@playwright/test";
 test.describe("Homepage", () => {
   test("should load and display correct title", async ({ page }) => {
     await page.goto("/");
-    await expect(page).toHaveTitle(/Create Next App/);
+    await expect(page).toHaveTitle(/Jeff | Grow Your Business/);
   });
 
   test("should display the main heading", async ({ page }) => {
